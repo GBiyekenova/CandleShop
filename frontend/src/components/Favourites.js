87dd848f63f7047270cc9d2b4f60010977efe2b1
Gulbike Biyekenova <gbknva@gmail.com>
2022-03-09 10:42:41 -0500

    get favourites info from backend

--- frontend/src/components/Favourites.js
+++ frontend/src/components/Favourites.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 import Navbar from "./Layout/Navbar";
 
 function Favourites() {
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`/favourites`)
+      .then((response) => {
+        console.log("response");
+        console.log(response.data.data);
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+      });
+  }, []);
+
   return (
     <div>
       <Navbar />
-      FAVOURITES
+      <div className="card">
+        {data &&
+          data.map((item) => (
+            <div onClick={() => navigate(`/candles/${item.id}`)}>
+              <div>
+                <img width="350" height="350" src={item.picture1} />
+              </div>
+              <div>
+                <h1>{item.name}</h1>
+                <div>${item.price}</div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
