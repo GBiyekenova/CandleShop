b914e16b1194eb80b57069315df7ff78ae781abf
Gulbike Biyekenova <gbknva@gmail.com>
2022-03-22 15:56:41 -0400

    completed functionality for favourites and cart

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
+import { CartContext } from "./CartContext";
 
 import Navbar from "./Layout/Navbar";
 import "../styles/Post.css";
 
 function Post() {
+  const [cart, setCart] = useContext(CartContext);
   const [data, setData] = useState(null);
   const { itemId } = useParams();
+  // const [cart, setCart] = useState([]);
+  // console.log(cart)
 
   useEffect(() => {
     axios
@@ -25,9 +29,50 @@ function Post() {
   }, []);
 
   const addToFav = () => {
+    axios
+      .post(`/candles/itemId`, { itemId })
+      .then((response) => {
+        console.log("response");
+        console.log(response);
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+      });
+  };
 
+  const removeFromFav = () => {
+    axios
+      .post("/candles/remove", { itemId })
+      .then((response) => {
+        console.log(response);
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+      });
+  };
 
-  }
+  const addToCart = (itemToAdd) => {
+    console.log("Add to cart!");
+    console.log(itemToAdd)
+    let inCart = false;
+    
+    let updatedCart;
+    updatedCart = cart.map((i) => {
+      if (i.id === itemToAdd.id) {
+        inCart = true;
+        i.amount += 1;
+        
+      }
+      return i;
+    });
+    if (inCart === false) {
+      console.log(itemToAdd)
+      updatedCart = [...cart, {...itemToAdd, amount: 1}];
+    }
+    setCart([...updatedCart]);
+  };
 
   return (
     <div>
@@ -35,15 +80,16 @@ function Post() {
       {data && (
         <div className="product-block">
           <div className="product-info">
-            <h1>
-              {data.name}
-              {data.id}
-            </h1>
+            <h1>{data.name}</h1>
             <div>{data.description}</div>
             <div>Price: {data.price}</div>
             <div>{data.quantity} in stock</div>
-            <button>Add To Cart</button>
-            <button onClick={addToFav}>Add to Favourites</button>
+            <button onClick={() => addToCart(data)}>Add To Cart</button>
+            {data.is_favourite ? (
+              <button onClick={removeFromFav}>Remove From Favourites</button>
+            ) : (
+              <button onClick={addToFav}>Add To Favourites</button>
+            )}
           </div>
 
           <div className="images">
