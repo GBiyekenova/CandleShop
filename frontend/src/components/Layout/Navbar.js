19937f88737ade279bd47ba71cb5232048bfcc2b
Gulbike Biyekenova <gbknva@gmail.com>
2022-03-22 15:58:52 -0400

    added a conter for cart

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+
+import { CartContext } from "../CartContext";
 
 import "../../styles/Navbar.css"
 
 function Navbar() {
+  const [cart] = useContext(CartContext);
+  
+  let cartNum = 0;
+  let cartCount = cart.map((item) => {
+    cartNum += item.amount;
+  })
+
   return (
     <ul>
       <li className="link"><Link to="/candles">Home</Link></li>
       <li className="link"> <Link to="/favourites">Favourites</Link></li>
-      <li className="link"><Link to="/cart">Cart</Link></li>
+      <li className="link"><Link to="/cart">Cart({cart ? cartNum : 0})</Link></li>
     </ul>
   );
 }
