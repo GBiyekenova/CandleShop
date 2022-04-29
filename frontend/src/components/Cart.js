dda6f6028d61c8aa0b61e5ed427db57cc3afd8e8
Gulbike Biyekenova <gbknva@gmail.com>
2022-04-29 11:54:47 -0400

    connected stripe.js payment

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -2,6 +2,8 @@ import React, { useContext, use } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Layout/Navbar";
 
+import cartCss from "../styles/Cart.css"
+
 import { CartContext } from "./CartContext";
 // import useLocalStorage from "./useLocalStorage";
 
@@ -12,35 +14,43 @@ function Cart() {
 
   const removeFromCart = (itemToRemove) => {
     console.log("Remove from Cart");
-    // let newCart = cart.map((item) => {
-    //   if (item.id === itemToRemove.id) {
-    //     if (item.amount > 1) {
-    //       item.amount -= 1;
-    //     } else if (item.amount === 1) {
-    //       cart.filter((el) => el.id !== itemToRemove.id);
-    //     }
-    //   }
-    // });
-    const newCart = cart.reduce((acc, val, index)=>{
-      if(val.id === itemToRemove.id){
-        if(val.amount > 1){
-          val.amount -= 1;
-          acc.push(val)
-        } 
-      } else {
-        acc.push(val)
-      }
-      return acc
-    },[])
-    //if i.id === item.id
-    //if i.amount > 1 then remove -1;
+    const newCart = cart.filter((item) => {
+      return item.id !== itemToRemove.id
+
+    })
     setCart([...newCart]);
   };
+
+  const decrease = (id) => {
+    const newCart = cart.reduce((acc, val, index) => {
+      if (val.id === id) {
+        if (val.amount > 1) {
+          val.amount -= 1;
+          acc.push(val);
+        }
+      } else {
+        acc.push(val);
+      }
+      return acc;
+    }, []);
+    setCart([...newCart]);
+  };
+
+  const increase = (id) => {
+    let currectCart = [...cart];
+    currectCart.map((item) => {
+      if (item.id === id) {
+          return (item.amount += 1);
+      }
+    });
+    setCart(currectCart);
+  };
+
   return (
     <div>
       <Navbar />
       <div className="card">
-        {cart &&
+        {cart && cart.length > 0 ?
           cart.map((item, index) => (
             <div key={index}>
               <div onClick={() => navigate(`/candles/${item.id}`)}>
@@ -52,10 +62,17 @@ function Cart() {
                   <div>${item.price}</div>
                 </div>
               </div>
+              <button onClick={() => decrease(item.id)}>-</button>
+              <div>{item.amount}</div>
+              <button onClick={() => increase(item.id)}>+</button>
               <button onClick={() => removeFromCart(item)}>REMOVE</button>
             </div>
-          ))}
-        <button>CHECKOUT</button>
+          )) : "Your cart is empty!"}
+        <form id="checkout" action="/create-checkout-session" method="POST">
+          <button width="50" height="10px" type="submit">
+            CHECKOUT
+          </button>
+        </form>
       </div>
     </div>
   );
