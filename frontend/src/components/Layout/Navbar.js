4139241bce941880b5220a9b35b7abb0adfd786a
Gulbike Biyekenova <gbknva@gmail.com>
2022-07-14 13:56:43 -0400

    added icons

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -1,23 +1,33 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 import { CartContext } from "../CartContext";
 
-import "../../styles/Navbar.css"
+import "../../styles/Navbar.css";
 
 function Navbar() {
   const [cart] = useContext(CartContext);
-  
+
   let cartNum = 0;
   let cartCount = cart.map((item) => {
     cartNum += item.amount;
-  })
+  });
 
   return (
     <ul>
-      <li className="link"><Link to="/candles">Home</Link></li>
-      <li className="link"> <Link to="/favourites">Favourites</Link></li>
-      <li className="link"><Link to="/cart">Cart({cart ? cartNum : 0})</Link></li>
+      <li className="link">
+        <Link to="/candles">Candle</Link>
+      </li>
+      <li className="link">
+        <Link to="/favourites"><FavoriteBorderIcon /></Link>
+      </li>
+      <li className="link">
+      
+        <Link to="/cart"> <ShoppingCartIcon />({cart ? cartNum : 0})</Link>
+      </li>
     </ul>
   );
 }
