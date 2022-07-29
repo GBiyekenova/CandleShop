921f0990a6d83ddf7561dc20647eb5c1d2e6e568
Gulbike Biyekenova <gbknva@gmail.com>
2022-07-29 10:51:57 -0400

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,27 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-
 import Favourites from "./components/Favourites";
 import Cart from "./components/Cart";
 import Post from "./components/Post";
+import AllPostings from "./components/AllPostings";
+import { CartProvider } from "./components/CartContext";
+
 
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
   return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<Post />}></Route>
-        <Route path="/favourites" element={<Favourites />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-      </Routes>
-        {/* <>{!data ? "Loading..." : data}</> */}
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/candles/:itemId" element={<Post />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/candles" element={<AllPostings />}></Route>
+          <Route path="/favourites" element={<Favourites />}></Route>
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
