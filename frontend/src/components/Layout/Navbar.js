196624833615edcf3edd266ede9d33dca5934827
Gulbike Biyekenova <gbknva@gmail.com>
2022-08-08 22:12:26 -0400

    navbar

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 // import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
@@ -7,10 +7,11 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { CartContext } from "../CartContext";
 
 import "../../styles/Navbar.css";
+import { CartModal } from "../CartModal";
 
 function Navbar() {
   const [cart] = useContext(CartContext);
-
+  const [openModal, setOpenModal] = useState(false);
   let cartNum = 0;
   let cartCount = cart.map((item) => {
     cartNum += item.amount;
@@ -28,10 +29,13 @@ function Navbar() {
       <li className="link">
         <Link to="/favourites"><FavoriteBorderIcon /></Link>
       </li>
-      <li className="link">
+      {/* <li className="link">
       
         <Link to="/cart"> <ShoppingCartIcon />({cartCount ? cartNum : 0})</Link>
-      </li>
+      </li> */}
+      
+      <button onClick={() => {setOpenModal(true)}}> <ShoppingCartIcon />({cartCount ? cartNum : 0})</button>
+      {openModal && <CartModal closeModal={setOpenModal}/>}
 
       </div>
     </ul>
