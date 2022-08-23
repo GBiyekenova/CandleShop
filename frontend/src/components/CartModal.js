63f711a8a6b7c619954435978055dfe92dbc403e
Gulbike Biyekenova <gbknva@gmail.com>
2022-08-23 10:36:43 -0400

    modal content

--- frontend/src/components/CartModal.js
+++ frontend/src/components/CartModal.js
@@ -1,20 +1,94 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 // import styled from "styled-components";
-import "../styles/CartModal.css"
+import "../styles/CartModal.css";
 
-export const CartModal = ({closeModal}) => {
+import { CartContext } from "./CartContext";
+
+const CartModal = ({ closeModal }) => {
+  const [cart, setCart] = useContext(CartContext);
+
+  const navigate = useNavigate();
+
+  const removeFromCart = (itemToRemove) => {
+    console.log("Remove from Cart");
+    const newCart = cart.filter((item) => {
+      return item.id !== itemToRemove.id;
+    });
+    setCart([...newCart]);
+  };
+
+  const decrease = (id) => {
+    const newCart = cart.reduce((acc, val, index) => {
+      if (val.id === id) {
+        if (val.amount > 1) {
+          val.amount -= 1;
+          acc.push(val);
+        }
+      } else {
+        acc.push(val);
+      }
+      return acc;
+    }, []);
+    setCart([...newCart]);
+  };
+
+  const increase = (id) => {
+    let currectCart = [...cart];
+    currectCart.map((item) => {
+      if (item.id === id) {
+        return (item.amount += 1);
+      }
+    });
+    setCart(currectCart);
+  };
   return (
-      
     <div className="modalBackground">
       <div className="modalContainer">
-        {/* <button>X</button> */}
-        <div className="header">
+
+        <div className="modal-header">
           <div>Cart</div>
           <div>Quantity</div>
-          <button onClick={() => {closeModal(false)}}>X</button>
+          <button
+          id="exit-btn"
+            onClick={() => {
+              closeModal(false);
+            }}
+          >
+            X
+          </button>
+
+        </div>
+        <div className="card">
+          {cart && cart.length > 0
+            ? cart.map((item, index) => (
+                <div key={index}>
+                  <div onClick={() => navigate(`/candles/${item.id}`)}>
+                    {/* <div>
+                      <img width="350" height="350" src={item.picture1} />
+                    </div> */}
+                    <div className="line">
+                      <h1 className="ctr">{item.name}</h1>
+                      <div className="ctr">${item.price}</div>
+                    </div>
+                  </div>
+                  <div className="ctr cart-btn">
+                    <button onClick={() => increase(item.id)}>+</button>
+                    <div>{item.amount}</div>
+                    <button onClick={() => decrease(item.id)}>-</button>
+                    <button onClick={() => removeFromCart(item)}>REMOVE</button>
+                  </div>
+                </div>
+              ))
+            : "Your cart is empty!"}
+          <form id="checkout" action="/create-checkout-session" method="POST">
+            <button id="checkout-btn" type="submit">
+              CHECKOUT
+            </button>
+          </form>
         </div>
       </div>
     </div>
-
-  )
-}
+  );
+};
+export default CartModal;
