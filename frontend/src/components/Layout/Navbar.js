8bd4264f8eb440217f53970dd7f1350b750b8e84
Gulbike Biyekenova <gbknva@gmail.com>
2022-08-24 19:33:38 -0400

    modal added

--- frontend/src/components/Layout/Navbar.js
+++ frontend/src/components/Layout/Navbar.js
@@ -2,12 +2,12 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 // import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 import { CartContext } from "../CartContext";
 
 import "../../styles/Navbar.css";
-import { CartModal } from "../CartModal";
+import  CartModal  from "../CartModal";
 
 function Navbar() {
   const [cart] = useContext(CartContext);
@@ -19,24 +19,31 @@ function Navbar() {
 
   return (
     <ul>
-      <li id="logo" className="link" >
+      <li id="logo" className="link">
         <Link to="/candles">Candl</Link>
       </li>
       <li className="link">
         <Link to="/candles">Shop</Link>
       </li>
       <div className="float-right">
-      <li className="link">
-        <Link to="/favourites"><FavoriteBorderIcon /></Link>
-      </li>
-      {/* <li className="link">
+        <li className="link">
+          <Link to="/favourites">
+            <FavoriteBorderIcon />
+          </Link>
+        </li>
+        {/* <li className="link">
       
         <Link to="/cart"> <ShoppingCartIcon />({cartCount ? cartNum : 0})</Link>
       </li> */}
-      
-      <button onClick={() => {setOpenModal(true)}}> <ShoppingCartIcon />({cartCount ? cartNum : 0})</button>
-      {openModal && <CartModal closeModal={setOpenModal}/>}
 
+        <button
+          onClick={() => {
+            setOpenModal(true);
+          }}
+        >
+          <ShoppingCartIcon />({cartCount ? cartNum : 0})
+        </button>
+        {openModal && <CartModal closeModal={setOpenModal} />}
       </div>
     </ul>
   );
