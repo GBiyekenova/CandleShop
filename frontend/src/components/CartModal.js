583dc91e390269968acaf60ff632bbb62ceb8faf
gbiyekenova <gbknva@gmail.com>
2023-04-13 10:54:09 -0400

    restructured cart table

--- frontend/src/components/CartModal.js
+++ frontend/src/components/CartModal.js
@@ -43,14 +43,15 @@ const CartModal = ({ closeModal }) => {
     setCart(currectCart);
   };
   return (
-    <div className="modalBackground">
+    <div
+      className="modalBackground"
+      // onClick={() => {
+      //   closeModal(false);
+      // }}
+    >
       <div className="modalContainer">
-          <div>Cart</div> <br/>
-        <div className="modal-header">
-          <div>Products</div>
-          <div>Price</div>
-          <div>Quantity</div>
-          <div>Order Summary</div>
+        <div id="cart-close">
+          <div>Cart</div>
           <button
             id="exit-btn"
             onClick={() => {
@@ -60,29 +61,32 @@ const CartModal = ({ closeModal }) => {
             X
           </button>
         </div>
-
+        <div className="modal-header">
+          <div>Products</div>
+          <div>Price</div>
+          <div>Quantity</div>
+          <div>Order Summary</div>
+        </div>
         <div id="cart-lf-rg">
           <div className="left-crt">
             {cart && cart.length > 0
               ? cart.map((item, index) => (
                   <div key={index}>
-                    <tr onClick={() => navigate(`/candles/${item.id}`)}>
-                      {/* <div> */}
-                        <td>
-                          <img width="100" height="100" src={item.picture1} />
-                        </td>
-                        {/* </div> */}
-                        <td>{item.name}</td>
-                        <td>${item.price}</td>
-                    </tr>
-
-                    <tr >{/*  className="ctr cart-btn" */}
+                    <tr onClick={() => navigate(`/candles/${item.id}`)} id="product-info">
+                      <td>
+                        <img width="100" height="100" src={item.picture1} />
+                        <div>{item.name}</div>
+                      </td>
+                      <td>${item.price}</td>
+                      <td>
+                       {/*  className="ctr cart-btn" */}
                       <button onClick={() => increase(item.id)}>+</button>
                       <div>{item.amount}</div>
                       <button onClick={() => decrease(item.id)}>-</button>
                       <button onClick={() => removeFromCart(item)}>
                         REMOVE
                       </button>
+                      </td>
                     </tr>
                   </div>
                 ))
@@ -102,130 +106,3 @@ const CartModal = ({ closeModal }) => {
   );
 };
 export default CartModal;
-
-// function CartModal(props) {
-//   const [activeTab, setActiveTab] = useState('quantity');
-//   const [cartItems, setCartItems] = useState([]);
-//   const [totalItems, setTotalItems] = useState(0);
-//   const [totalPrice, setTotalPrice] = useState(0);
-
-//   // Function to open the cart modal
-//   const openModal = () => {
-//     document.getElementById('cart-modal').style.display = 'block';
-//   };
-
-//   // Function to close the cart modal
-//   const closeModal = () => {
-//     document.getElementById('cart-modal').style.display = 'none';
-//   };
-
-//   // Function to switch between tabs
-//   const openTab = (event, tabName) => {
-//     const tabContents = document.getElementsByClassName('tabcontent');
-//     for (let i = 0; i < tabContents.length; i++) {
-//       tabContents[i].style.display = 'none';
-//     }
-//     const tabLinks = document.getElementsByClassName('tablinks');
-//     for (let i = 0; i < tabLinks.length; i++) {
-//       tabLinks[i].className = tabLinks[i].className.replace(' active', '');
-//     }
-//     document.getElementById(tabName).style.display = 'block';
-//     event.currentTarget.className += ' active';
-//     setActiveTab(tabName);
-//   };
-
-//   // Function to add an item to the cart
-//   const addItem = (item) => {
-//     const existingItemIndex = cartItems.findIndex((cartItem) => cartItem.id === item.id);
-//     if (existingItemIndex >= 0) {
-//       const updatedCartItems = [...cartItems];
-//       updatedCartItems[existingItemIndex].quantity++;
-//       setCartItems(updatedCartItems);
-//     } else {
-//       setCartItems([...cartItems, { ...item, quantity: 1 }]);
-//     }
-//   };
-
-//   // Function to remove an item from the cart
-//   const removeItem = (itemId) => {
-//     const updatedCartItems = cartItems.filter((cartItem) => cartItem.id !== itemId);
-//     setCartItems(updatedCartItems);
-//   };
-
-//   // Function to update the total items and total price
-//   const updateTotals = () => {
-//     let newTotalItems = 0;
-//     let newTotalPrice = 0;
-//     cartItems.forEach((cartItem) => {
-//       newTotalItems += cartItem.quantity;
-//       newTotalPrice += cartItem.quantity * cartItem.price;
-//     });
-//     setTotalItems(newTotalItems);
-//     setTotalPrice(newTotalPrice);
-//   };
-
-//   // Function to handle clicking the checkout button
-//   const handleCheckout = () => {
-//     // Do something with the cart data (e.g. send to server)
-//     // ...
-//     // Close the modal
-//     closeModal();
-//   };
-
-//   return (
-//     <>
-//       {/* Button to open the cart modal */}
-//       <button onClick={openModal}>Open Cart</button>
-
-//       {/* Cart modal */}
-//       <div id="cart-modal" className="modal">
-//         <div className="modal-content">
-//           <div className="modal-header">
-//             <span className="close" onClick={closeModal}>&times;</span>
-//             <h2>Your Cart</h2>
-//           </div>
-//           <div className="modal-body">
-//             {/* Quantity tab */}
-//             <div className="tab">
-//               <button className={activeTab === 'quantity' ? 'tablinks active' : 'tablinks'} onClick={(event) => openTab(event, 'quantity')}>Quantity</button>
-//               <div id="quantity" className={activeTab === 'quantity' ? 'tabcontent active' : 'tabcontent'}>
-//                 {cartItems.length > 0 ? (
-//                   <ul className="cart-items">
-//                     {cartItems.map((cartItem) => (
-//                       <li key={cartItem.id}>
-//                         <div>{cartItem.name}</div>
-//                         <div>Quantity: {cartItem.quantity}</div>
-//                         <div>Price: ${cartItem.price}</div>
-//                         <button onClick={() => removeItem(cartItem.id)}>Remove</button>
-//                       </li>
-//                     ))}
-//                   </ul>
-//                 ) : (
-//                   <p>Your cart is empty.</p>
-//                 )}
-//                 <div className="total">
-//                   <div>Total Items: {totalItems}</div>
-//                   <div>Total Price: ${totalPrice.toFixed(2)}</div>
-//                 </div>
-//               </div>
-//             </div>
-
-//             {/* Order summary tab */}
-//             <div className="tab">
-//               <button className={activeTab === 'summary' ? 'tablinks active' : 'tablinks'} onClick={(event) => openTab(event, 'summary')}>Order Summary</button>
-//               <div id="summary" className={activeTab === 'summary' ? 'tabcontent active' : 'tabcontent'}>
-//                 <div className="total">
-//                   <div>Total Items: {totalItems}</div>
-//                   <div>Total Price: ${totalPrice.toFixed(2)}</div>
-//                 </div>
-//                 <button onClick={handleCheckout}>Checkout</button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default CartModal;
