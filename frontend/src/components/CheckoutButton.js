01818438cd2c7389f5b5c291cd4e2834e79e85d1
gbiyekenova <gbknva@gmail.com>
2023-05-17 21:56:55 -0400

    separated button logic

---
+++ frontend/src/components/CheckoutButton.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import axios from "axios";
+
+import { CartContext } from "./CartContext";
+
+const checkout = (cart) => {
+  axios
+    .post("/create-checkout-session", { cart })
+    .then((response) => {
+      console.log("SUCCESS", response);
+      window.location.href = response.data.url;
+    })
+    .catch((err) => {
+      console.log("ERROR", err);
+    });
+};
+
+const CheckoutButton = () => {
+  const [cart, setCart] = useContext(CartContext);
+  return (
+    <div
+      onClick={() => {
+        checkout(cart);
+      }}
+      id="checkout"
+    >
+      <button id="checkout-btn">CHECKOUT</button>
+    </div>
+  );
+};
+
+export default CheckoutButton;
