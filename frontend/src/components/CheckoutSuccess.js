ff4e0a0c5301ea3177c4beca138a256548e85f9e
gbiyekenova <gbknva@gmail.com>
2023-05-23 22:48:42 -0400

    success checkout page

--- frontend/src/components/CheckoutSuccess.js
+++ frontend/src/components/CheckoutSuccess.js
@@ -1,8 +1,14 @@
-const CheckoutSuccess = () => {
-    return (
-        <h2>Checkout Success</h2>
-    )
+import React from "react";
+import "../styles/CheckoutSuccess.css"
 
-}
+const CheckoutSuccess = () => {
+  return (
+    <div className="thankyou-page">
+      <h2>Checkout Success</h2>
+      <h2>Thank you for your purchase!</h2>
+      <a href="http://localhost:3000/candles">Continue Shopping</a>
+    </div>
+  );
+};
 
 export default CheckoutSuccess;
